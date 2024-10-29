e336e8e7fe73c5fa9fcc5e0285d5537f8a00c18e
Gabriel Jezewski <gabrieljezewski@MacBook-Pro-de-Gabriel.local>
2024-10-29 10:56:40 -0300

    feat: adiciona modal e integracao com api logout

--- src/screens/Login/Login.screen.tsx
+++ src/screens/Login/Login.screen.tsx
@@ -71,7 +71,7 @@ export default function LoginScreen() {
               value={value}
               error={errors?.password && errors?.password?.message}
               secureTextEntry={isVisiblePassword}
-              // maxLength={8}
+              maxLength={8}
               autoCapitalize="none"
             />
           )}
